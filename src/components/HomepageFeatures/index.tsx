256c6c258b004e9a612c855f33216491b3ecf651
Adam Pazderka <pazderka.adam@protonmail.com>
2025-03-01 11:10:52 +0100

    first commit

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,31 @@
+import Heading from '@theme/Heading';
+import clsx from 'clsx';
+import type { ReactNode } from 'react';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  description: ReactNode;
+};
+
+function Feature({title, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+        </div>
+      </div>
+    </section>
+  );
+}
